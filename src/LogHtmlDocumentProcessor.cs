e34106aa97880a9c917c85a494ed3e1f55bcaba8
Arjen.van.Veen <Arjen.van.Veen@75db680c-6849-0410-8f82-9bc0dc489441>
2014-03-20 11:57:37 +0000

    Other: Added new files
    git-svn-id: https://svn.efocusgroep.nl/svn/efocus_dotnet/eFocus/Modules/Efocus.Sitecore.LuceneWebSearch/Branches/1.3@93257 75db680c-6849-0410-8f82-9bc0dc489441

---
+++ src/LogHtmlDocumentProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using BoC.Logging;
+
+namespace Efocus.Sitecore.LuceneWebSearch
+{
+    class LogHtmlDocumentProcessor : HtmlDocumentProcessor
+    {
+        private readonly ILogger _logger;
+
+        public LogHtmlDocumentProcessor()
+        {
+        }
+
+        public LogHtmlDocumentProcessor(ILogger logger, Dictionary<IEnumerable<char>, IEnumerable<char>> filterTextRules,
+            Dictionary<IEnumerable<char>, IEnumerable<char>> filterLinksRules)
+            : base(filterTextRules, filterLinksRules)
+        {
+            _logger = logger;
+        }
+
+        protected override void AddStepToCrawler(NCrawler.Crawler crawler, NCrawler.PropertyBag propertyBag, string normalizedLink, string link)
+        {
+            if (_logger != null) _logger.DebugFormat("Crawler:AddStepToCrawler | {0}", normalizedLink);
+
+            base.AddStepToCrawler(crawler, propertyBag, normalizedLink, link);
+        }
+    }
+}
---
+++ src/Support/CrawlFinishedPipelineArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NCrawler;
+
+namespace Efocus.Sitecore.LuceneWebSearch.Support
+{
+    [Serializable]
+    public class CrawlFinishedEventArgs : CrawlerEventArgs
+    {
+        public CrawlFinishedEventArgs(Crawler crawler)
+            : base(crawler)
+        {
+        }
+    }
+}
