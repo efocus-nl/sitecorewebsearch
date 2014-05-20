4f9326c04a2c132c75a3fef37b11d0f35dc1a14b
Guido van Tricht <guido.van.tricht@efocus.nl>
2014-05-20 07:47:15 +0200

    Added first version of TextListBlock, ContentButtonBlock and added corporate.main placeholder

---
+++ src/Init/InitContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoC.InversionOfControl;
+using BoC.Logging;
+
+namespace Efocus.Sitecore.LuceneWebSearch.Init
+{
+    public class InitContainer: IContainerInitializer
+    {
+        private readonly IDependencyResolver _resolver;
+
+        public InitContainer(IDependencyResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public void Execute()
+        {
+            _resolver.RegisterType<ILogger,SiteCoreLogger>();
+        }
+    }
+}
